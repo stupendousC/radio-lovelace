a65cb611b2b10820dd61e80ac708a8352472b4c1
stupendousC <CarolineWuKaplan@gmail.com>
2019-12-02 22:46:58 -0800

    converted RadioSet to a class component, still passing props successfully

--- src/components/RadioSet.js
+++ src/components/RadioSet.js
@@ -1,46 +1,46 @@
 import React from 'react';
 import "./styles/RadioSet.css";
-
 import Playlist from './Playlist';
 
-const RadioSet = (props) => {
-  // console.log(`Radio set for ${props.tracks.length} tracks`);
-
-  const playlists = {
-    morningTracks: props.tracks.slice(0, props.tracks.length / 2),
-    eveningTracks: props.tracks.slice(props.tracks.length / 2, props.tracks.length)
-  };
-
-  const radioSetCB_Fav = (id, favorite) => {
-    console.log( `RadioSet -> App... toggle on ${id} newFav = ${favorite}`);
-    
-    props.parentCB_Fav(id, favorite);
+export default class RadioSet extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      morningTracks: props.tracks.slice(0, props.tracks.length / 2),
+      eveningTracks: props.tracks.slice(props.tracks.length / 2, props.tracks.length),
+      parentCB_Fav: props.parentCB_Fav
+    }
   }
 
-  const radioSetCB_Switch = (id, playlistName) => {
+  radioSetCB_Fav = (id, favorite) => {
+    console.log( `RadioSet -> App... toggle on ${id} newFav = ${favorite}`);
+    
+    this.state.parentCB_Fav(id, favorite);
+  }
+
+  radioSetCB_Switch = (id, playlistName) => {
     console.log(`Radioset has received info: ${id} & ${[playlistName]}`);
     
   }
 
-
-  return (
-    <div className="radio-set">
-      <section className="radio-set--playlist-container">
-        <Playlist
-          side="Morning"
-          tracks={playlists.morningTracks}
-          parentCB_Fav={radioSetCB_Fav}
-          parentCB_Switch={radioSetCB_Switch}
-        />
-        <Playlist
-          side="Evening"
-          tracks={playlists.eveningTracks}
-          parentCB_Fav={radioSetCB_Fav}
-          parentCB_Switch={radioSetCB_Switch}
-        />
-      </section>
-    </div>
-  );
-};
-
-export default RadioSet;
+  render() {
+    return (
+      <div className="radio-set">
+        <section className="radio-set--playlist-container">
+          <Playlist
+            side="Morning"
+            tracks={this.state.morningTracks}
+            parentCB_Fav={this.radioSetCB_Fav}
+            parentCB_Switch={this.radioSetCB_Switch}
+          />
+          <Playlist
+            side="Evening"
+            tracks={this.state.eveningTracks}
+            parentCB_Fav={this.radioSetCB_Fav}
+            parentCB_Switch={this.radioSetCB_Switch}
+          />
+        </section>
+      </div>
+    );
+  }
+}
